290ceb2bc65573750db09b0e22f035c19d34d701
Zackarin Necesito <zacknecesito@gmail.com>
2020-11-16 21:33:30 -0600

    Wrote to Local DB via Register form

--- injections/routes.py
+++ injections/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template, url_for, flash, redirect
-from injections import app
+from injections import app, db, bcrypt
 from injections.models import User
 from injections.forms import RegistrationForm, LoginForm
 
@@ -18,6 +18,10 @@ def home():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
         flash(f'Account created for {form.email.data}!', 'success')
         return redirect(url_for('home'))
     return render_template('register.html', title='Fill This Out', form=form)
